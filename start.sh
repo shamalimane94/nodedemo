0df57192446df05c012ca703301a5bf2f20f29fd
shamalimane94 <shamalimane94@gmail.com>
2019-11-17 20:35:58 +0530

    backend added

--- start.sh
+++ start.sh
@@ -13,4 +13,4 @@ fi
 
 docker build -t my_app_backend .
 
-docker  run -it -p 4000:4000 --name my_app_backend_container my_app_backend
+docker  run -itd -p 4000:4000 --name my_app_backend_container my_app_backend
