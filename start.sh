8c159444628ee5c7bb1f31bd97afc3c31964a84b
shamalimane94 <shamalimane94@gmail.com>
2019-11-17 20:38:13 +0530

    backend added

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "welcome server..."
 npm install
-if [ $(docker container ls -q --filter name=my_app_backend_container) != '']; then
+if [ $(docker container ls -a -q --filter name=my_app_backend_container) != '']; then
  docker container stop my_app_backend_container
  docker container rm my_app_backend_container 
 fi
 
-if [ $(docker image ls -q --filter reference=my_app_backend) != '' ];then
+if [ $(docker image ls -a -q --filter reference=my_app_backend) != '' ];then
   docker image rm my_app_backend
 fi
 
