764935084ba83a3e8e6848122485056f50faa41f
shamalimane94 <shamalimane94@gmail.com>
2019-11-17 21:49:53 +0530

    category ..

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 var product=require('./product')
 const app = express()
-app.router('/product',product)
+app.use('/product',product)
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); 
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
